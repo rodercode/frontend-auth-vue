e0b23f0fcbbb9d13648f3ad0ee2e58ab98da2aa4
rodercode <rodersnow@gmail.com>
2023-06-07 16:17:52 +0200

    implement login page

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,6 +1,36 @@
 <template>
-  <div class="login">
-    <h1>Login Page</h1>
+  <div class="container-register">
+    <form class="form-register">
+      <header class="form-header">
+        <h1>Login</h1>
+      </header>
+      <section class="section-input">
+        <label class="label-username">Username</label>
+        <input
+          class="input-username"
+          placeholder="Type your username..."
+          type="username"
+        />
+      </section>
+
+      <section class="section-input">
+        <label class="label-password">Password</label>
+        <input
+          class="input-password"
+          placeholder="Enter a password..."
+          type="password"
+        />
+      </section>
+
+      <p class="info-link">
+        Already have an account? Sign in
+        <router-link class="link" :to="{ name: 'login' }">here</router-link>
+      </p>
+      <div class="container-btns">
+        <button class="btn">Register new account</button>
+        <button class="btn">Proceed as guest user</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -8,6 +38,50 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "LoginView",
+  name: "RegisterView",
 });
 </script>
+
+<style scoped>
+.container-register {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.form-register {
+  background: rgb(207, 201, 201);
+  width: 30%;
+  padding: 1.5em;
+}
+.form-header {
+  margin-bottom: 2em;
+}
+
+.section-input {
+  margin: 0 1em 2em 1em;
+}
+.info-link {
+  margin: 0 0 0.5em 1em;
+}
+
+input {
+  width: 100%;
+  padding: 1em;
+}
+
+label {
+  text-align: left;
+  display: inline-block;
+  font-size: 24px;
+}
+
+.container-btns{
+  display: flex;
+}
+.btn {
+  width: 50%;
+  padding: 0.75em;
+  margin-left: 1em;
+}
+</style>
